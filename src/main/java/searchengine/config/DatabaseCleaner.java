1750b38797dee5369d7df50bf7f57a9174c73f85
максим <136228428+maksim578@users.noreply.github.com>
2025-04-15 18:53:37 +0500

    Update DatabaseCleaner.java

--- src/main/java/searchengine/config/DatabaseCleaner.java
+++ src/main/java/searchengine/config/DatabaseCleaner.java
@@ -15,32 +15,32 @@ public class DatabaseCleaner {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-//    @PreDestroy
-//    @Transactional
-//    public void cleanDatabase() {
-//        try {
-//            List<String> tables = jdbcTemplate.queryForList("SHOW TABLES", String.class);
-//
-//            int rowsDeletedSite = deleteIfExists("site", tables);
-//            int rowsDeletedPage = deleteIfExists("page", tables);
-//            int rowsDeletedLemma = deleteIfExists("lemma", tables);
-//            int rowsDeletedIndex = deleteIfExists("index_data", tables);
-//
-//            logger.info("Удалено записей: site = {}, page = {}, lemma = {}, index_data = {}",
-//                    rowsDeletedSite, rowsDeletedPage, rowsDeletedLemma, rowsDeletedIndex);
-//
-//            logger.info("База данных успешно очищена.");
-//        } catch (Exception e) {
-//            logger.error("Ошибка при очистке базы данных: ", e);
-//        }
-//    }
-//
-//    private int deleteIfExists(String tableName, List<String> tables) {
-//        if (tables.contains(tableName)) {
-//            return jdbcTemplate.update("DELETE FROM " + tableName);
-//        } else {
-//            logger.warn("Таблица '{}' отсутствует в базе данных, пропускаем удаление.", tableName);
-//            return 0;
-//        }
-//    }
+    @PreDestroy
+    @Transactional
+    public void cleanDatabase() {
+        try {
+            List<String> tables = jdbcTemplate.queryForList("SHOW TABLES", String.class);
+
+            int rowsDeletedSite = deleteIfExists("site", tables);
+            int rowsDeletedPage = deleteIfExists("page", tables);
+            int rowsDeletedLemma = deleteIfExists("lemma", tables);
+            int rowsDeletedIndex = deleteIfExists("index_data", tables);
+
+            logger.info("Удалено записей: site = {}, page = {}, lemma = {}, index_data = {}",
+                    rowsDeletedSite, rowsDeletedPage, rowsDeletedLemma, rowsDeletedIndex);
+
+            logger.info("База данных успешно очищена.");
+        } catch (Exception e) {
+            logger.error("Ошибка при очистке базы данных: ", e);
+        }
+    }
+
+    private int deleteIfExists(String tableName, List<String> tables) {
+        if (tables.contains(tableName)) {
+            return jdbcTemplate.update("DELETE FROM " + tableName);
+        } else {
+            logger.warn("Таблица '{}' отсутствует в базе данных, пропускаем удаление.", tableName);
+            return 0;
+        }
+    }
 }
